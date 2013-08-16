abf7308ecb5fe6a63ccdcaa873247f6ffa588915
sonyfe25cp@gmail.com <sonyfe25cp@gmail.com>
2013-08-16 17:38:45 +0800

    new add

--- src/mapper/SentenceMapper.java
+++ src/mapper/SentenceMapper.java
@@ -1,9 +1,20 @@
 package mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import source.Sentence;
 
 public interface SentenceMapper {
 
-	public Sentence getFirstSentence();
+	
+	public Sentence getSentenceById(int id);
+	
+	public Sentence getSentenceBySentenceIdAndDocName(@Param("sentenceId") int sentenceId, String docName);
+	
+	public List<Sentence> getSentencesByDocumentName(String docName);
+	
+	public List<Sentence> getSentencesByEventName(String eventName);
 
 }
---
+++ src/service/SentenceService.java
@@ -0,0 +1,13 @@
+package service;
+
+public class SentenceService {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
