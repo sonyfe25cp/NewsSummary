e9f4c856f62b20b23e8eb46b4b0b766f2c9517f7
sonyfe25cp@gmail.com <sonyfe25cp@gmail.com>
2013-12-31 14:51:49 +0800

    basic controller demo

---
+++ src/action/CommonAction.java
@@ -0,0 +1,16 @@
+package action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class CommonAction {
+
+	@RequestMapping("/")
+	public String index(){
+		return "/index";
+	}
+}
